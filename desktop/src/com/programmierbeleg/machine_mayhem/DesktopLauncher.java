9b5b4cd2423db17052f849e6cc4ef2115f3958af
C0denator <129959584+C0denator@users.noreply.github.com>
2023-07-02 20:37:13 +0200

    Add files via upload

--- desktop/src/com/programmierbeleg/machine_mayhem/DesktopLauncher.java
+++ desktop/src/com/programmierbeleg/machine_mayhem/DesktopLauncher.java
@@ -12,28 +12,20 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class DesktopLauncher {
 
 	private static int foregroundFPS=120;
-	private static boolean vollbildAn=false;
+	private static boolean vollbildAn=true;
 	private static int breite=1920;
 	private static int höhe=1080;
 
 
 	public static void main (String[] arg) {
 		System.out.println("Programm gestartet - Daumen drücken!");
-		TexturePacker.Settings settings = new TexturePacker.Settings();
-		settings.maxHeight=4096;
-		settings.maxWidth=4096;
-		settings.edgePadding=true;
-		settings.filterMin=Texture.TextureFilter.Nearest;
-		settings.filterMag=Texture.TextureFilter.Nearest;
-		TexturePacker.process(settings, "assets/Texturen","assets","texturenAtlas");
-		//Diesen Block vor dem Build entfernen!!!
 
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(foregroundFPS);
 		config.useVsync(true);
 		config.setResizable(false);
 		config.setTitle("Machine Mayhem");
-		config.setWindowIcon(Files.FileType.Local,"assets/Texturen/robot_1.png");
+		config.setWindowIcon(Files.FileType.Internal,"assets/Texturen/robot_1.png");
 		if(vollbildAn){
 			config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		}else{
